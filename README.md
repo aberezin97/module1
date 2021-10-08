68e85fa7b00c5670830e875be1970179f0e46113
Alexander Berezin <alexanderberezin97@yandex.ru>
2021-10-08 21:20:17 +0300

    Отредактировал README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+Netlify: https://cranky-blackwell-1b0f56.netlify.app/
+
+Dev: npm run dev
+Build: npm run build
+Express: npm run start
--- src/pages/scripts/main.js
+++ src/pages/scripts/main.js
@@ -1,5 +1,5 @@
 // Логика модальных окон
-let modals = document.getElementsByClassName("modal");
+const modals = document.getElementsByClassName("modal");
 window.addEventListener("click", (event) => {
   for (let i = 0; i < modals.length; i++) {
     if (event.target == modals[i]) {
